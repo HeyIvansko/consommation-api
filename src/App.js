f54ac8f90164661521e1ac65d19a9cd915d7bb32
Ivansko <ivanxsoumah@gmail.com>
2023-05-11 17:23:19 +0000

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  });
+
+ 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+  
+  function handleInputChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(newUser => {
+        
+        setData([...data, newUser]);
+       
+        setFormData({
+          name: '',
+          email: ''
+        });
+      });
+  }
+
+ 
+  function handleClearHistory() {
+    setData([]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Liste des utilisateurs</h2>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>{user.name} ({user.email})</li>
+        ))}
+      </ul>
+      <button onClick={handleClearHistory}>Effacer l'historique</button>
+      <h2>Ajouter un utilisateur</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nom:
+          <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+        </label>
+        <br />
+        <button type="submit">Ajouter</button>
+      </form>
     </div>
   );
 }
